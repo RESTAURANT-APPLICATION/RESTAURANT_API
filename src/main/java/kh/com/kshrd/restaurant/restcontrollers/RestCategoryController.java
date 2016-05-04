d8ee8e8a2983d2e69a233024e6bd20368c85bf85
darapenhchet <darapenhchet@gmail.com>
2016-05-04 10:35:25 +0700

    THRID COMMIT

--- src/main/java/kh/com/kshrd/restaurant/restcontrollers/RestCategoryController.java
+++ src/main/java/kh/com/kshrd/restaurant/restcontrollers/RestCategoryController.java
@@ -1,6 +1,8 @@
 package kh.com.kshrd.restaurant.restcontrollers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
@@ -16,8 +18,13 @@ public class RestCategoryController {
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<Map<String, Object>> findAllCategories(){
 		Map<String, Object> model = new HashMap<String, Object>();
+		List<String> strs = new ArrayList<String>();
+		strs.add("PIRANG");
+		strs.add("KOKPHENG");
+		model.put("DATA", strs);
 		model.put("MESSAGE", "WELCOME TO RESTAURANT APPLICATION");
 		model.put("CODE", "0000");
+		model.put("PAGINATION", null);
 		return new ResponseEntity<Map<String, Object>>(model, HttpStatus.OK);
 	}
 }
--- src/main/java/kh/com/kshrd/restaurant/restcontrollers/RestRestaurantController.java
+++ src/main/java/kh/com/kshrd/restaurant/restcontrollers/RestRestaurantController.java
@@ -1,8 +1,10 @@
 package kh.com.kshrd.restaurant.restcontrollers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/v1/api/restaurants")
 public class RestRestaurantController {
 
 }
